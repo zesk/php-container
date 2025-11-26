bd46f667493850868ba1fcf8419408b15b61fd50
Kent Davidson <kent@marketacumen.com>
2025-11-25 22:58:35 -0500

    workaround temporary issue with `decorate wrap`

--- src/Application.php
+++ src/Application.php
@@ -9,30 +9,30 @@ use zesk\Response;
 
 class Application extends ApplicationBase
 {
-    /**
-     * @return void
-     * @throws \zesk\Exception\ClassNotFound
-     * @throws \zesk\Exception\SemanticsException
-     */
-    protected function afterConfigure(): void
-    {
-        $this->router->addRoute('index', [
-            'method' => $this->homeHandler(...),
-            'arguments' => ['{request}'],
-        ]);
-        $this->router->addRoute('favicon.ico', [
-            'method' => $this->faviconHandler(...),
-            'arguments' => ['{request}'],
-        ]);
-    }
+	/**
+	 * @return void
+	 * @throws \zesk\Exception\ClassNotFound
+	 * @throws \zesk\Exception\SemanticsException
+	 */
+	protected function afterConfigure(): void
+	{
+		$this->router->addRoute('index', [
+			'method' => $this->homeHandler(...),
+			'arguments' => ['{request}'],
+		]);
+		$this->router->addRoute('favicon.ico', [
+			'method' => $this->faviconHandler(...),
+			'arguments' => ['{request}'],
+		]);
+	}
 
-    public function homeHandler(Request $request): Response
-    {
-        return $this->application->responseFactory($request)->json()->setData(['message' => 'Hello world!', 'path' => $request->path()]);
-    }
+	public function homeHandler(Request $request): Response
+	{
+		return $this->application->responseFactory($request)->json()->setData(['message' => 'Hello world!', 'path' => $request->path()]);
+	}
 
-    public function faviconHandler(Request $request): Response
-    {
-        return $this->application->responseFactory($request)->setStatus(HTTP::STATUS_NO_CONTENT)->setContentType("image/ico")->setContent("");
-    }
+	public function faviconHandler(Request $request): Response
+	{
+		return $this->application->responseFactory($request)->setStatus(HTTP::STATUS_NO_CONTENT)->setContentType('image/ico')->setContent('');
+	}
 }
